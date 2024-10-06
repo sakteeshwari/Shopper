3cf08dab0beda5585a133457dc556a76261831d7
sakteeshwari <sakthijessy26@gmail.com>
2024-10-06 12:15:01 +0530

    added shop correctly

--- frontend/src/components/Popular/Popular.jsx
+++ frontend/src/components/Popular/Popular.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import "./Popular.css"
-// import data_product from "../Assets/Frontend_Assets/data.js"
+import data_product from "../Assets/Frontend_Assets/data.js"
 import Item from '../Item/Item'
 import { useEffect } from 'react'
 
@@ -20,7 +20,7 @@ const Popular = () => {
         <hr />
         <div className='popular-item'>
             {
-                popularProducts.map((item,i)=>{
+                data_product.map((item,i)=>{
                     return <Item className="item" key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} ></Item>
                 })
             }
